317a7bb12b9488957152f9b86890d0a09bff89ac
Xeno <xenocidechan@gmail.com>
2020-10-14 07:19:31 +0200

    Moving to laptop

--- connect.php
+++ connect.php
@@ -1,14 +1,14 @@
 <?php
 $servername = "localhost";
-$username = "username";
+$username = "root";
 $password = "password";
 
-// Create connection
-$conn = new mysqli($backend, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=backend", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-echo "Connected successfully";
 ?>
