30e5f0fc889e0c17acc464d72e3e2be4ab1edf57
Xeno <xenocidechan@gmail.com>
2021-02-16 12:21:31 +0100

    didn't commit correctly

--- list_read.php
+++ list_read.php
@@ -7,8 +7,9 @@ while ($data = $stmt->fetch())
     echo '<div class="list">'
             .'<p class="id">'.$data['id'].'</p>'
             .'<p class="card-title">'.$data['name'].'</p>'
-            .'<form action="list_update.php?id='.$data['id'].'&name='.$data['name'].'" method="post">
+            .'<form action="list_update.php" method="post">
             List Nameupdt: <input type="text" name="name" /><br><br>
+            <input type="hidden" name="id" value="'.$data['id'].'" />
             <input type="submit" />
             </form>'
             .'<a class="edit buttons" href="../Back-End/list_delete.php?id='.$data['id'].'">Delete</button></a>'
--- list_update.php
+++ list_update.php
@@ -1,20 +1,17 @@
 <?
 include "connect.php";
-var_dump($_POST);
+
 // initialize variables for the insert query
 echo '<br>';
-if (isset($_GET['id'])) {
-    echo 'made it this far 1 (nailed)<br>';
-    if (!empty($_GET)) {
+if (isset($_POST['id'])) {
+    if (!empty($_POST)) {
         $id = isset($_POST['id']) ? $_POST['id'] : NULL;
         $name = isset($_POST['name']) ? $_POST['name'] : '';
-        echo 'made it this far 2 (nailed)<br>';
         // Update the record
-        $stmt = $pdo->prepare('UPDATE lists SET "id" = $id, "name" = $name, WHERE id = ?');
-        $stmt->execute(['id'=>$id, 'name'=>$name, $_GET['id']]);
+        $stmt = $pdo->prepare('UPDATE lists SET `id` = :id, `name` = :name WHERE id = :id');
+        $stmt->execute([':id'=>$id, ':name'=>$name]);
         $msg = 'Updated Successfully!';
     }
-    echo 'made it this far 3 (not nailed)<br>';
     header('Location: index.php');
 } else{
     exit('No ID specified!');
